992997d89ebf9448ed853d7a2a1f5b160afb7955
Felipe300 <caracoloro@gmail.com>
2022-06-09 17:48:24 -0400

    feat: Add Qr code component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"Light-gray": "#d6e2f0", // hsl(212, 45%, 89%)
+				"Grayish-blue": "#7b879d", // hsl(220, 15%, 55%)
+				"Dark-blue": "#1f3251" // hsl(218, 44%, 22%)
+			},
+			fontFamily: {
+				sans: ["Outfit", "sans-serif"]
+			},
+			boxShadow: {
+				'special': '0px 0px 10px hsla(0, 0%, 0%, 0.5)',
+			}
+		},
 	},
 	plugins: [],
 }
